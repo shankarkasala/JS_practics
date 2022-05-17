08f0254f251bd0d9d9b100b06e0ca8d41d822ea0
shankarkasala <umashankarno66@gmail.com>
2022-05-17 17:44:15 +0530

    commit

---
+++ Algo/sort.js
@@ -0,0 +1,57 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let numberToInsert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numberToInsert) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = numberToInsert;
+  }
+}
+//merg sort
+const arr = [8, 20, -2, 4, -6];
+insertionSort(arr);
+console.log(arr); // [-6, -2, 4, 8, 20]
+
+function mergesort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const mid = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, mid);
+  const rightArr = arr.slice(mid);
+  return merge(mergesort(leftArr), mergesort(rightArr));
+}
+
+function merge(leftArr, rightArr) {
+  const sortedArr = [];
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+  const resultArr = [...sortedArr, ...leftArr, ...rightArr];
+  return resultArr;
+}
+
+console.log(mergesort(arr)); // [-6, -2, 4, 8, 20]
+
+function quickSort(arr) {
+  if (arr.length < 2) return arr;
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+console.log(quickSort(arr)); // [-6, -2, 4, 8, 20]
